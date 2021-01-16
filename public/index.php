f562be1f1817cf97ce9c5c5bdb5fa36b955d5b77
Jake Suellentrop <jake@lifeboatcreative.com>
2021-01-16 14:46:35 -0600

    updates logo and site screenshots

--- public/index.php
+++ public/index.php
@@ -20,7 +20,8 @@
 				<!-- Header -->
 					<header id="header">
 						<div class="logo">
-							<span class="icon fa-gem"></span>
+							<!-- <span class="icon fa-gem"></span> -->
+							<img src="/images/wichita-flag-sun.png" alt="Wichita">
 						</div>
 						<div class="content">
 							<div class="inner">
@@ -43,7 +44,8 @@
 						<!-- Intro -->
 							<article id="intro">
 								<h2 class="major">Promo Depot</h2>
-								<span class="image main"><img src="images/pic01.jpg" alt="" /></span>
+								<p class="align-right"><a href="https://4mypromo.com" target="_blank">Visit Website</a></p>
+								<span class="image main"><img src="images/promodepot.png" alt="Promo Depot" /></span>
 								<p>Aenean ornare velit lacus, ac varius enim ullamcorper eu. Proin aliquam facilisis ante interdum congue. Integer mollis, nisl amet convallis, porttitor magna ullamcorper, amet egestas mauris. Ut magna finibus nisi nec lacinia. Nam maximus erat id euismod egestas. By the way, check out my <a href="#work">awesome work</a>.</p>
 								<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis dapibus rutrum facilisis. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Etiam tristique libero eu nibh porttitor fermentum. Nullam venenatis erat id vehicula viverra. Nunc ultrices eros ut ultricies condimentum. Mauris risus lacus, blandit sit amet venenatis non, bibendum vitae dolor. Nunc lorem mauris, fringilla in aliquam at, euismod in lectus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. In non lorem sit amet elit placerat maximus. Pellentesque aliquam maximus risus, vel sed vehicula.</p>
 							</article>
@@ -51,7 +53,8 @@
 						<!-- Work -->
 							<article id="work">
 								<h2 class="major">Bio-Center Laboratory</h2>
-								<span class="image main"><img src="images/pic02.jpg" alt="" /></span>
+								<p class="align-right"><a href="https://biocenterlab.lifeboatcreative.com" target="_blank">Visit Website</a></p>
+								<span class="image main"><img src="images/biocenterlab.png" alt="Bio-Center Laboratory" /></span>
 								<p>Adipiscing magna sed dolor elit. Praesent eleifend dignissim arcu, at eleifend sapien imperdiet ac. Aliquam erat volutpat. Praesent urna nisi, fringila lorem et vehicula lacinia quam. Integer sollicitudin mauris nec lorem luctus ultrices.</p>
 								<p>Nullam et orci eu lorem consequat tincidunt vivamus et sagittis libero. Mauris aliquet magna magna sed nunc rhoncus pharetra. Pellentesque condimentum sem. In efficitur ligula tate urna. Maecenas laoreet massa vel lacinia pellentesque lorem ipsum dolor. Nullam et orci eu lorem consequat tincidunt. Vivamus et sagittis libero. Mauris aliquet magna magna sed nunc rhoncus amet feugiat tempus.</p>
 							</article>
@@ -59,7 +62,8 @@
 						<!-- About -->
 							<article id="about">
 								<h2 class="major">American Oil & Gas Reporter</h2>
-								<span class="image main"><img src="images/pic03.jpg" alt="" /></span>
+								<p class="align-right"><a href="https://aogr.com" target="_blank">Visit Website</a></p>
+								<span class="image main"><img src="images/aogr.png" alt="American Oil & Gas Reporter" /></span>
 								<p>Lorem ipsum dolor sit amet, consectetur et adipiscing elit. Praesent eleifend dignissim arcu, at eleifend sapien imperdiet ac. Aliquam erat volutpat. Praesent urna nisi, fringila lorem et vehicula lacinia quam. Integer sollicitudin mauris nec lorem luctus ultrices. Aliquam libero et malesuada fames ac ante ipsum primis in faucibus. Cras viverra ligula sit amet ex mollis mattis lorem ipsum dolor sit amet.</p>
 							</article>
 
