eef1c6ea8f4ad7111f2b5514061d249035cef4a4
Jake Suellentrop <jake@lifeboatcreative.com>
2021-01-16 13:55:49 -0600

    set up theme with bg

---
+++ src/js/app.js
@@ -0,0 +1,6 @@
+window.jQuery = require('jquery')
+window.browser = require('./browser')
+window.breakpoints = require('./breakpoints')
+
+require('./util')
+require('./main')
---
+++ webpack.mix.js
@@ -0,0 +1,4 @@
+let mix = require('laravel-mix')
+
+mix.js('src/js/app.js', 'public/js');
+mix.sass('src/sass/main.scss', 'public/css')
